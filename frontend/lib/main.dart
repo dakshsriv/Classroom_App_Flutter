656e67cbfe6bdce7407286c6d777ba81a08564f1
Daksh Srivastava <daksh.srivastava@gmail.com>
2023-01-01 13:31:11 -0500

    CMake fixed

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const LoginPage());
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class Info {
+  final int userId;
+  final int accountType;
+
+  const Info({
+    required this.userId,
+    required this.accountType,
+  });
+
+  factory Info.fromJson(Map<String, dynamic> json) {
+    return Info(
+      userId: json['id'],
+      accountType: json['type'],
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _LoginPageState extends State<LoginPage> {
   int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,5 +60,20 @@ class _HomePageState extends State<HomePage> {
                 });
               })
             ]))));
+  }
+}
+
+Future<Info> fetchLogin() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Info.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load info');
   }
 }
--- frontend/windows/flutter/generated_plugin_registrant.cc
+++ frontend/windows/flutter/generated_plugin_registrant.cc
@@ -6,14 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-<<<<<<< HEAD
-#include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
-=======
-
-void RegisterPlugins(flutter::PluginRegistry* registry) {
->>>>>>> 37bf92e (Beginning frontend development)
 }
